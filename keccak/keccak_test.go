0367582218d937f8c0d6705a820a081bb8345cd3
Coda Hale <coda.hale@gmail.com>
2026-01-29 23:01:28 -0700

    Refactor Keccak tests to use fuzzing and known vectors
    Replace `TestCompliance` and `TestCompliance12` with `FuzzF1600` and `FuzzP1600` to verify consistency between generic and assembly implementations.
    Add `TestP1600`, `TestF1600`, and `TestF1600Generic` to verify outputs against known test vectors.

--- keccak/keccak_test.go
+++ keccak/keccak_test.go
@@ -2,41 +2,99 @@ package keccak //nolint:testpackage // testing internals
 
 import (
 	"bytes"
-	"math/rand"
+	"crypto/sha3"
+	"encoding/hex"
 	"testing"
-	"time"
 )
 
-func TestCompliance(t *testing.T) {
-	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-	var state1, state2 [200]byte
+func TestP1600(t *testing.T) {
+	var state [200]byte
+	P1600(&state)
 
-	for i := range 100 {
-		rng.Read(state1[:])
-		copy(state2[:], state1[:])
+	if got, want := hex.EncodeToString(state[:]), "1786a7b938545e8e1ed059f2506acdd9351fa952c6e7b887c5e0e4cd67e09310455ad9f290ab33b0451adda8722fa7e09c2f6714aa8037c51d075100f547dd3ecc8a170c311da3b3a0aa5792a586b5799bf9b1b33d7c4abc93678ae66340876866250e2e33036c5cda30f0b90212aa9c9f7acf2b789a3b5f2379ae61e0c136e5ec873cb718b6e96dc28a9170f1d1be2ab724edda53bdab6a5ae12e2c6a41c1bfaf5209b936e0cfc6d76070dc17365045e47a9fc2b21156627a64302cdb7136d41ca02c22760dfdcf"; got != want {
+		t.Errorf("P1600(0*200) = %s, want = %s", got, want)
+	}
+}
+
+func TestF1600(t *testing.T) {
+	var state [200]byte
+	F1600(&state)
+
+	if got, want := hex.EncodeToString(state[:]), "e7dde140798f25f18a47c033f9ccd584eea95aa61e2698d54d49806f304715bd57d05362054e288bd46f8e7f2da497ffc44746a4a0e5fe90762e19d60cda5b8c9c05191bf7a630ad64fc8fd0b75a933035d617233fa95aeb0321710d26e6a6a95f55cfdb167ca58126c84703cd31b8439f56a5111a2ff20161aed9215a63e505f270c98cf2febe641166c47b95703661cb0ed04f555a7cb8c832cf1c8ae83e8c14263aae22790c94e409c5a224f94118c26504e72635f5163ba1307fe944f67549a2ec5c7bfff1ea"; got != want {
+		t.Errorf("F1600(0*200) = %s, want = %s", got, want)
+	}
+}
+
+func TestF1600Generic(t *testing.T) {
+	t.Run("12 rounds", func(t *testing.T) {
+		var state [200]byte
+		f1600Generic(&state, 12)
+
+		if got, want := hex.EncodeToString(state[:]), "1786a7b938545e8e1ed059f2506acdd9351fa952c6e7b887c5e0e4cd67e09310455ad9f290ab33b0451adda8722fa7e09c2f6714aa8037c51d075100f547dd3ecc8a170c311da3b3a0aa5792a586b5799bf9b1b33d7c4abc93678ae66340876866250e2e33036c5cda30f0b90212aa9c9f7acf2b789a3b5f2379ae61e0c136e5ec873cb718b6e96dc28a9170f1d1be2ab724edda53bdab6a5ae12e2c6a41c1bfaf5209b936e0cfc6d76070dc17365045e47a9fc2b21156627a64302cdb7136d41ca02c22760dfdcf"; got != want {
+			t.Errorf("P1600(0*200) = %s, want = %s", got, want)
+		}
+	})
+
+	t.Run("24 rounds", func(t *testing.T) {
+		var state [200]byte
+		f1600Generic(&state, 24)
+
+		if got, want := hex.EncodeToString(state[:]), "e7dde140798f25f18a47c033f9ccd584eea95aa61e2698d54d49806f304715bd57d05362054e288bd46f8e7f2da497ffc44746a4a0e5fe90762e19d60cda5b8c9c05191bf7a630ad64fc8fd0b75a933035d617233fa95aeb0321710d26e6a6a95f55cfdb167ca58126c84703cd31b8439f56a5111a2ff20161aed9215a63e505f270c98cf2febe641166c47b95703661cb0ed04f555a7cb8c832cf1c8ae83e8c14263aae22790c94e409c5a224f94118c26504e72635f5163ba1307fe944f67549a2ec5c7bfff1ea"; got != want {
+			t.Errorf("F1600(0*200) = %s, want = %s", got, want)
+		}
+	})
+}
+
+func FuzzF1600(f *testing.F) {
+	drbg := sha3.NewSHAKE128()
+	_, _ = drbg.Write([]byte("Keccak-f[1600]"))
+	for range 10 {
+		var state [200]byte
+		_, _ = drbg.Read(state[:])
+		f.Add(state[:])
+	}
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) != 200 {
+			t.Skip()
+		}
+
+		var state1, state2 [200]byte
+		copy(state1[:], data)
+		copy(state2[:], data)
 
 		F1600(&state1)            // Should use ASM
 		f1600Generic(&state2, 24) // Reference
 
 		if !bytes.Equal(state1[:], state2[:]) {
-			t.Errorf("iteration %d: generic (24 rounds) mismatch ASM", i)
+			t.Errorf("Keccak-f[1600](%x) = %x, want = %x", data, state1, state2)
 		}
-	}
+	})
 }
 
-func TestCompliance12(t *testing.T) {
-	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-	var state1, state2 [200]byte
+func FuzzP1600(f *testing.F) {
+	drbg := sha3.NewSHAKE128()
+	_, _ = drbg.Write([]byte("Keccak-p[1600,12]"))
+	for range 10 {
+		var state [200]byte
+		_, _ = drbg.Read(state[:])
+		f.Add(state[:])
+	}
 
-	for i := range 100 {
-		rng.Read(state1[:])
-		copy(state2[:], state1[:])
+	f.Fuzz(func(t *testing.T, data []byte) {
+		if len(data) != 200 {
+			t.Skip()
+		}
+
+		var state1, state2 [200]byte
+		copy(state1[:], data)
+		copy(state2[:], data)
 
 		P1600(&state1)            // Should use ASM
 		f1600Generic(&state2, 12) // Reference
 
 		if !bytes.Equal(state1[:], state2[:]) {
-			t.Errorf("iteration %d: generic (12 rounds) mismatch ASM", i)
+			t.Errorf("Keccak-p[1600,12](%x) = %x, want = %x", data, state1, state2)
 		}
-	}
+	})
 }
